1855b58a8f2f3bdf4eeb731b7d6300fcb6587718
TroyAnthonyBarker <trobar@alu.edu.gva.es>
2023-05-29 17:30:30 +0200

    Pantallas Empleados. Pantalla ListarEmpleados creada. Pantalla ModificarEmpleado creada. Pantalla VerEmpleado creada.

---
+++ src/ModificarEmpleado.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class ModificarEmpleado {
+    private JPanel panelPrincipal;
+    private JTextField txtFldNif;
+    private JTextField txtFldNombre;
+    private JTextField txtFldApellido1;
+    private JTextField txtFldApellido2;
+    private JTextField txtFldDireccion;
+    private JTextField txtFldEmail;
+    private JTextField txtFldDepartamento;
+    private JTextField txtFldTelefono;
+    private JLabel logoMARCOSTV;
+    private JButton confirmarButton;
+    private JButton cancelarButton;
+}
