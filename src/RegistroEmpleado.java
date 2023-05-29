40e744e84cc3979d9b6d99ecbf7096cfcf0ae950
RaquelGuerreroPerucho <raquelguerreroperucho@gmail.com>
2023-05-29 13:01:06 +0200

    Pantalla registro empleado MARCOS TV

---
+++ src/RegistroEmpleado.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class RegistroEmpleado {
+    private JPanel JPanelGeneralEmpleado;
+    private JTextField introNIF;
+    private JTextField introNombre;
+    private JTextField introApellido1;
+    private JTextField introApellido2;
+    private JTextField introDireccion;
+    private JTextField introEmail;
+    private JTextField inroDepartamento;
+    private JTextField introTelefono;
+    private JLabel logoMARCOSTV;
+    private JButton cancelarButton;
+    private JButton crearButton;
+public RegistroEmpleado() {
+    crearButton.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    });
+}
+}
