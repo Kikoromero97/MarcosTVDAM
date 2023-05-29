77ef50f19a1ab4d952c69485abc4c1c83cea248e
TroyAnthonyBarker <trobar@alu.edu.gva.es>
2023-05-29 17:58:45 +0200

    Pantalla VerDepartamento creada.

---
+++ src/VerDepartamento.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class VerDepartamento {
+    private JPanel panelPrincipal;
+    private JLabel logoMARCOSTV;
+    private JComboBox comboBox1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextArea textArea1;
+    private JButton areaDeEmpleadosButton;
+    private JButton modificarButton;
+    private JButton eliminarButton;
+}
