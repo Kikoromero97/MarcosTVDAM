98a783d3221acb7a4699d0cda69f1b78514d2c97
RaquelGuerreroPerucho <raquelguerreroperucho@gmail.com>
2023-05-30 14:24:58 +0200

    PANTALLA EPISODIO

---
+++ src/Episodio.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class Episodio {
+    private JPanel panelEpisodio;
+    private JPanel JPanelTemporada;
+    private JTextField introNombreEpisodio;
+    private JTextField introFechaLanzamientoEpisodio;
+    private JButton eliminarButtonEpisodio;
+    private JButton editarButtonEpisodio;
+    private JButton salirButtonEpisodio;
+    private JTextField introNEpisodio;
+    private JTextField IntroDuracionEpisodio;
+    private JTextField introDescripcion;
+}
