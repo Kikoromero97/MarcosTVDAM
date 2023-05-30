d1ea952d633c3779c67f300266b4d1baa6c84ef6
RaquelGuerreroPerucho <raquelguerreroperucho@gmail.com>
2023-05-30 14:13:29 +0200

    PANTALLA TEMPORADA

---
+++ src/Temporada.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Temporada {
+    private JPanel panel1;
+    private JPanel JPanelTemporada;
+    private JTextField introNombre;
+    private JTextField introNumero;
+    private JButton eliminarButton;
+    private JButton editarButton;
+    private JButton salirButton;
+}
