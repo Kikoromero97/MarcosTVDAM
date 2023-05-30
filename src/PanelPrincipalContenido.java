473816c2f1a5fd0970ea1c97f7e1c59bf1f66b4a
RaquelGuerreroPerucho <raquelguerreroperucho@gmail.com>
2023-05-30 12:35:03 +0200

    Panel principal contenido creado.

---
+++ src/PanelPrincipalContenido.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class PanelPrincipalContenido {
+    private JPanel JPanelPrincipal;
+    private JPanel JPanelBotones;
+    private JButton paisButton;
+    private JButton idiomaButton;
+    private JButton peliculaButton;
+    private JButton categoriaButton;
+    private JButton serieButton;
+    private JButton documentalButton;
+    private JButton salirButton;
+    private JLabel logoMarcosTV;
+}
