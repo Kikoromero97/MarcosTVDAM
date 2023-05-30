745d12a9847b0be17a2a638f0fe51ae28d5f1f49
RaquelGuerreroPerucho <raquelguerreroperucho@gmail.com>
2023-05-30 13:03:48 +0200

    Pantalla crear pelicula

---
+++ src/crearPelicula.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+
+public class crearPelicula {
+    private JPanel JPanelCrearPelicula;
+    private JPanel JPanelIntroDatosPelicula;
+    private JFormattedTextField formattedTextField1;
+    private JFormattedTextField formattedTextField2;
+    private JFormattedTextField formattedTextField3;
+    private JFormattedTextField formattedTextField4;
+    private JFormattedTextField formattedTextField5;
+    private JFormattedTextField formattedTextField6;
+    private JFormattedTextField formattedTextField7;
+    private JFormattedTextField formattedTextField8;
+    private JFormattedTextField formattedTextField9;
+    private JFormattedTextField formattedTextField10;
+    private JFormattedTextField formattedTextField11;
+    private JFormattedTextField formattedTextField12;
+    private JLabel logoMarcosTV;
+    private JButton crearButton;
+    private JButton cancelarButton;
+}
