10983ff00fea8a5ab39fb67b63ee5c0b4199daca
RaquelGuerreroPerucho <raquelguerreroperucho@gmail.com>
2023-05-30 13:39:09 +0200

    PANTALLAS VER SERIE, PELUCULA Y DOCUMENTAL

---
+++ src/verDocumental.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class verDocumental {
+    private JPanel JPanelVerDocumental;
+    private JPanel JPanelBotones;
+    private JButton buttonCancelDocumental;
+    private JButton buttonVolverDocumental;
+    private JButton crearButtonDocumental;
+    private JLabel logoMarcosTV;
+    private JButton buscarButtonDocumental;
+    private JTextField introDocumental;
+    private JTable table1Documental;
+}
