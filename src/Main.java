682b1fd00aad1224bfdb34b41a13f482d3ec2b36
TroyAnthonyBarker <trobar@alu.edu.gva.es>
2023-06-07 11:41:35 +0200

    Cambio de rama

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-
+        if (UserManager.hasSession()){
+            UserManager.loadSession();
+            SwingUtilities.invokeLater(new Runnable() {
+                @Override
+                public void run() {
+                    JFrame frame = new menuPrincipal();
+                }
+            });
+        } else {
+            SwingUtilities.invokeLater(new Runnable() {
+                @Override
+                public void run() {
+                    //JFrame frame = new inicioSesion();
+                }
+            });
+        }
     }
 }
--- src/menuPrincipal.java
+++ src/menuPrincipal.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class menuPrincipal {
+public class menuPrincipal extends JFrame{
     private JPanel JpanelPrincipalOpciones;
     private JLabel logoMarcosTV;
     private JButton areaDepartamentoButton;
@@ -11,4 +13,27 @@ public class menuPrincipal {
     private JPanel JPanelBotonesPrincipales;
     private JPanel JPanelBotonSalir;
     private JButton areaDirectoresButton;
+
+
+    public menuPrincipal(){
+        super("MARCOS TV");
+        setSize(700, 500);
+        setVisible(true);
+        setLocationRelativeTo(null);
+        setContentPane(JpanelPrincipalOpciones);
+
+
+        areaUsuarioButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                SwingUtilities.invokeLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        JDialog infoUsuario = new InfoUsuario();
+                    }
+                });
+            }
+        });
+    }
 }
