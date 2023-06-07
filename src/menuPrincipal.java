d84ffe67c9d672b0a7a8fc61e34fde2f6a33608c
TroyAnthonyBarker <trobar@alu.edu.gva.es>
2023-06-07 12:41:31 +0200

    Menu principal Listro para revisiones

--- src/menuPrincipal.java
+++ src/menuPrincipal.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class menuPrincipal {
+public class menuPrincipal extends JFrame{
     private JPanel JpanelPrincipalOpciones;
     private JLabel logoMarcosTV;
     private JButton areaDepartamentoButton;
@@ -11,4 +13,69 @@ public class menuPrincipal {
     private JPanel JPanelBotonesPrincipales;
     private JPanel JPanelBotonSalir;
     private JButton areaDirectoresButton;
+
+    public menuPrincipal() {
+        super("MARCOS TV");
+        setSize(700, 500);
+        setVisible(true);
+        setContentPane(JpanelPrincipalOpciones);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        switch (UserManager.user.getRol()){
+            case atc -> {
+                areaDepartamentoButton.setEnabled(false);
+                areaContenidoButton.setEnabled(false);
+                areaDirectoresButton.setEnabled(false);
+            }
+            case rrhh -> {
+                areaUsuarioButton.setEnabled(false);
+                areaContenidoButton.setEnabled(false);
+                areaDirectoresButton.setEnabled(false);
+            }
+            case calidad -> {
+                areaUsuarioButton.setEnabled(false);
+                areaDepartamentoButton.setEnabled(false);
+            }
+        }
+        salirButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                UserManager.exitSession();
+                inicioSesion.mostrarInicioSession();
+                dispose();
+            }
+        });
+        areaUsuarioButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // SeleccionUser.mostrarUsuarios();
+                dispose();
+            }
+        });
+        areaContenidoButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // PanelPrincipalContenido.mostrarpanelPrincipalContenido();
+                dispose();
+            }
+        });
+        areaDepartamentoButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // VerDepartamento.mostrarDepartamentos();
+                dispose();
+            }
+        });
+        areaDirectoresButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // VerDirector.mostrarDirector();
+                dispose();
+            }
+        });
+    }
+    public static void  mostrarMenuPrincipal(){
+        JFrame frame = new menuPrincipal();
+    }
 }
