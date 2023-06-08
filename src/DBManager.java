632f18ff7547393df07444a03433a6feabd9e9c6
Kikoromero97 <kikoromero1997@gmail.com>
2023-06-09 00:27:49 +0200

    DBManager preparado

--- src/DBManager.java
+++ src/DBManager.java
@@ -5,16 +5,13 @@ public abstract class DBManager {
     private static Connection conn = null;
 
     // Configuración de la conexión a la base de datos
-    private static final String DB_HOST = "192.168.18.208\\SQLEXPRESS";
+    private static final String DB_HOST = "192.168.7.208\\SQLEXPRESS";
     private static final String DB_NAME = "databaseName=MARCOSTV";
     private static final String DB_PORT = "1433";
     private static final String DB_TRUST_SERVER = "TrustServerCertificate=true";
     private static final String DB_URL = "jdbc:sqlserver://"+ DB_HOST + ":" + DB_PORT +";" + DB_TRUST_SERVER + ";" + DB_NAME;
-
-
     // Configuración del usuario de la base de datos
-
-    private static String user = "Carmen";
+    private static String user = "programa";
     private static String pass = "1234";
 
     // Configuración de mensajes de respuesta
